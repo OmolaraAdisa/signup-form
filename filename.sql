09c587e276467e2b0018a6a29eb3ed7229341d3c
omolara <adisaomolara.oa@gmail.com>
2018-10-30 14:35:46 +0100

    updated form with mysql script

---
+++ filename.sql
@@ -0,0 +1,18 @@
+	1	idPrimary	int(50)			No	None	AUTO_INCREMENT	Change Change	Drop Drop	
+More
+	2	firstname	varchar(50)	utf8_unicode_ci		No	None		Change Change	Drop Drop	
+More
+	3	lastname	varchar(50)	utf8_unicode_ci		No	None		Change Change	Drop Drop	
+More
+	4	email	varchar(50)	utf8_unicode_ci		No	None		Change Change	Drop Drop	
+More
+	5	password	varchar(50)	utf8_unicode_ci		No	None		Change Change	Drop Drop	
+More
+	6	phoneno	varchar(14)	utf8_unicode_ci		No	None		Change Change	Drop Drop	
+More
+	7	gender	text	utf8_unicode_ci		No	None		Change Change	Drop Drop	
+More
+	8	country	text	utf8_unicode_ci		No	None		Change Change	Drop Drop	
+More
+	9	date	datetime(6)			No	None		Change Change	Drop Drop	
+Moregit
