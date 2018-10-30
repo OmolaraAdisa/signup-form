3d3099fa37c9aa0ffaba4bb46f3a19d772000eb8
omolara <adisaomolara.oa@gmail.com>
2018-10-30 14:52:35 +0100

    form update

--- filename.sql
+++ filename.sql
@@ -1,9 +1,11 @@
-		id Primary	int(50)		AUTO_INCREMENT
+		Create table users
+        insert into table values:
+        id Primary	int(50)		AUTO_INCREMENT
 
 		firstname	varchar(50)	
 
 		lastname	varchar(50)	
-        
+
 		email	varchar(50)	
 
 		password	varchar(50)		
