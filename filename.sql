7af90f01741d8b6ac10b185191dd33169812c9b3
omolara <adisaomolara.oa@gmail.com>
2018-10-30 14:44:31 +0100

    form update

--- filename.sql
+++ filename.sql
@@ -1,18 +1,18 @@
-	1	idPrimary	int(50)			No	None	AUTO_INCREMENT	Change Change	Drop Drop	
-More
-	2	firstname	varchar(50)	utf8_unicode_ci		No	None		Change Change	Drop Drop	
-More
-	3	lastname	varchar(50)	utf8_unicode_ci		No	None		Change Change	Drop Drop	
-More
-	4	email	varchar(50)	utf8_unicode_ci		No	None		Change Change	Drop Drop	
-More
-	5	password	varchar(50)	utf8_unicode_ci		No	None		Change Change	Drop Drop	
-More
-	6	phoneno	varchar(14)	utf8_unicode_ci		No	None		Change Change	Drop Drop	
-More
-	7	gender	text	utf8_unicode_ci		No	None		Change Change	Drop Drop	
-More
-	8	country	text	utf8_unicode_ci		No	None		Change Change	Drop Drop	
-More
-	9	date	datetime(6)			No	None		Change Change	Drop Drop	
-Moregit
+		id Primary	int(50)		AUTO_INCREMENT
+
+		firstname	varchar(50)	
+
+		lastname	varchar(50)	
+        
+		email	varchar(50)	
+
+		password	varchar(50)		
+
+    	phoneno	varchar(14)	
+
+		gender	text		
+
+		country	text		
+
+		date	datetime(6)				
+
